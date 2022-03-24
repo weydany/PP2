3bb3d70491196d458af6728707292f485c3bda6d
Dayana Khairanova <d_khairanova@kbtu.kz>
2022-03-25 01:16:49 +0600

    lab6

--- Lab6/file handling/c.py
+++
@@ -1,9 +0,0 @@
-import os
-
-if os.path.isdir('C:\\Users\\acer\\OneDrive\\Рабочий стол\\decode'):
-    print('Exists')
-    for dirpath, dirnames, filenames in os.walk('C:\\Users\\acer\\OneDrive\\Рабочий стол\\decode'):
-        for dirname in dirnames:
-            print('Каталог: ', os.path.join(dirpath, dirname))
-else:
-    print('Not exists')
